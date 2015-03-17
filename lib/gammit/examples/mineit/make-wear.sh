fcbc6fc788807a586bb7af8359ee73c5e2a450c9
Alexis Laferrière <alexis.laf@xymus.net>
2015-03-17 15:59:20 -0400

    remote
    Signed-off-by: Alexis Laferrière <alexis.laf@xymus.net>

---
+++ lib/gammit/examples/mineit/make-wear.sh
@@ -0,0 +1,6 @@
+adb forward tcp:4444 localabstract:/adb-hub
+adb connect localhost:4444
+
+nitc --dir bin/ src/android_wear_remote.nit || exit 1
+adb -s localhost:4444 install -r bin/android_wear_remote.apk
+adb -s localhost:4444 adb logcat
